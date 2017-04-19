7f68d39eb8d355051f7e960809a86712b3c47922
PabloXyu <zygmunciak@gmail.com>
2017-04-19 22:05:00 +0200

    linearLayoutHeight = displayHeight - actionBarHeight - statusBarHeight
    All heights finally well counted.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.android.quizapp;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.ViewGroup;
+import android.util.TypedValue;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import static java.security.AccessController.getContext;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    int displayHeight;
+    class Constants {
+        public static final int NUM_QUESTIONS = 4;
+    }
+
 
     // prints the "text" of a TextView (xml-named "textViewIdName")
     private void printOnTextView(String textViewIdName, String text) {
@@ -22,15 +28,31 @@ public class MainActivity extends AppCompatActivity {
      * This method sets the height of a LinearLayout (xml-named "linearLayoutIdName")
      * to dip height of the device screen.
      */
-    private void SetDisplayHeightToLinearLayout(String linearLayoutIdName) {
+    private void SetLinearLayoutHeight(String linearLayoutIdName) {
+
+        // displayHeight counted:
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int displayHeight = displayMetrics.heightPixels;
-        printOnTextView("header_Q1",""+displayHeight);
-        LinearLayout linearLayout=(LinearLayout)findViewById(getResources().getIdentifier(linearLayoutIdName,"id",getPackageName()));
 
+        // actionBarHeight counted:
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(android.R.attr.actionBarSize, typedValue, true);
+        int actionBarHeight = getResources().getDimensionPixelSize(typedValue.resourceId);
+
+        // statusBarHeight counted:
+        int statusBarHeight = getResources().getDimensionPixelSize(getResources().getIdentifier("status_bar_height", "dimen", "android"));
+
+        // linearLayoutHeight counted:
+        int linearLayoutHeight = displayHeight - actionBarHeight - statusBarHeight;
+
+        //printOnTextView("header_Q1",""+linearLayoutHeight+", "+displayHeight+", "+actionBarHeight+", "+statusBarHeight);
+        // ^ above: to see instead of "Question #1" title the heights of: Question Layout, displayWindow, actionBar, statusBar.
+
+        // linearLayoutHeight set:
+        LinearLayout linearLayout=(LinearLayout)findViewById(getResources().getIdentifier(linearLayoutIdName,"id",getPackageName()));
         android.view.ViewGroup.LayoutParams layoutParams = linearLayout.getLayoutParams();
-        layoutParams.height = displayHeight;
+        layoutParams.height = linearLayoutHeight;
         linearLayout.setLayoutParams(layoutParams);
     }
 
@@ -39,7 +61,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SetDisplayHeightToLinearLayout("layout_Q1");
+        SetLinearLayoutHeight("layout_Q1");
+        SetLinearLayoutHeight("layout_Q2");
+        SetLinearLayoutHeight("layout_Q3");
+        SetLinearLayoutHeight("layout_Q4");
+
     }
 
 
